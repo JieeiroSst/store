6274a1e103510119a8f2a58f4040ba9a3c3d1162
mq <luumanhquan.91@gmail.com>
2020-12-06 21:12:36 +0700

    .

--- server.go
+++ server.go
@@ -2,26 +2,35 @@ package main
 
 import (
 	"github.com/casbin/casbin/v2"
+	gormadapter "github.com/casbin/gorm-adapter/v3"
 	"github.com/gin-gonic/gin"
 	"github.com/jieeiro/api/commands"
 	"github.com/jieeiro/api/utils/authz"
-	gormadapter "github.com/casbin/gorm-adapter/v3"
 	"log"
 )
 
 func main()  {
 	server:=gin.Default()
 
-	a,err:=gormadapter.NewAdapter("postgresql","postgresql://root:@1234:5432/hospital?sslmode=disable")
+	adapter,err:=gormadapter.NewAdapter("postgresql","postgresql://root:@1234:5432/hospital?sslmode=disable")
 	if err!=nil{
 		log.Println(err)
 	}
 
-	e, _ :=casbin.NewEnforcer("authz_model.conf", a)
+	enforcer, _ :=casbin.NewEnforcer("authz_model.conf", adapter)
 
-	server.Use(authz.NewAuthorizer(e))
+	server.Use(authz.NewAuthorizer(enforcer))
+
+	_ = enforcer.LoadPolicy()
 
 	commands.InitCommand()
+	_ = enforcer.SavePolicy()
 
-	server.Run()
+	// Do permission checking
+	//enforcer.Enforce("alice", "data1", "write")
+	// Do some mutations
+	//enforcer.AddPolicy("alice", "data2", "write")
+	//enforcer.RemovePolicy("alice", "data1", "write")
+
+	_ = server.Run()
 }
