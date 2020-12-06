7f269fcafef5d6699a999ffc4fdf2b93bff85a89
mq <luumanhquan.91@gmail.com>
2020-12-06 21:08:23 +0700

    fix authencation

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/casbin/casbin/v2 v2.19.4
+	github.com/casbin/gorm-adapter/v3 v3.0.4 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
--- server.go
+++ server.go
@@ -5,12 +5,19 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/jieeiro/api/commands"
 	"github.com/jieeiro/api/utils/authz"
+	gormadapter "github.com/casbin/gorm-adapter/v3"
+	"log"
 )
 
 func main()  {
 	server:=gin.Default()
 
-	e, _ :=casbin.NewEnforcer("authz_model.conf", "authz_policy.csv")
+	a,err:=gormadapter.NewAdapter("postgresql","postgresql://root:@1234:5432/hospital?sslmode=disable")
+	if err!=nil{
+		log.Println(err)
+	}
+
+	e, _ :=casbin.NewEnforcer("authz_model.conf", a)
 
 	server.Use(authz.NewAuthorizer(e))
 
